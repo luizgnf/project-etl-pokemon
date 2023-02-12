3b3e5ed9cabeed3291a14bde78531af151ff2fc2
Luiz Gustavo <fagundes.luizg@gmail.com>
2023-02-12 14:52:06 -0300

    Include saving history and branch operator mechanism

--- dags/dag_pokedex_pokemon.py
+++ dags/dag_pokedex_pokemon.py
@@ -19,7 +19,8 @@ dag_full = create_extraction_dags(
     api_request_type = full_request,
     api_endpoint = 'pokemon',
     extract_keys = ["id"],
-    postgres_columns_list = ["rawdata"]
+    postgres_columns_list = ["rawdata"],
+    history_saving = True
 )
 
 dag_hourly = create_extraction_dags(
